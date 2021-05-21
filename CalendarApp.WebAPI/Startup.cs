c10f091aefbf7106ff0333b78ae5f1bef953870f
Jimoh Habeeb <habeebjimoh@hotmail.com>
2021-05-21 09:40:46 +0100

    Included SwaggerUI in production

--- CalendarApp.WebAPI/Startup.cs
+++ CalendarApp.WebAPI/Startup.cs
@@ -1,4 +1,5 @@
 using CalendarApp.Data;
+
 using CalendarApp.WebAPI.MyServices;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -42,9 +43,14 @@ namespace CalendarApp.WebAPI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CalendarApp.WebAPI v1"));
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "CalendarApp.WebAPI v1");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseHttpsRedirection();
 
